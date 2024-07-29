16da9d505be72cce263362dc7c45fc9d31777fa1
devandref <yoandredepaula@gmail.com>
2024-07-29 19:47:24 -0300

    init develop

--- src/main/java/io/github/devandref/authUser/AuthUserApplication.java
+++ src/main/java/io/github/devandref/authUser/AuthUserApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AuthUserApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthUserApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AuthUserApplication.class, args);
+    }
 
 }
